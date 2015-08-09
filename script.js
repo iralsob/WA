ad8e79f27529ce85a9dc48ba162633003eef930d
iralsob <iralsob@gmail.com>
2015-08-09 21:42:49 +0300

    цикл

--- script.js
+++ script.js
@@ -70,14 +70,11 @@ function init() {
 	}
 }
 function finishedLoading(bufferList) {
-	// Create two sources and play them both together.
-	var source1 = context.createBufferSource();
-	var source2 = context.createBufferSource();
-	source1.buffer = bufferList[0];
-	source2.buffer = bufferList[1];
-
-	source1.connect(context.destination);
-	source2.connect(context.destination);
-	source1.start(0);
-	source2.start(0);
+  // Create two sources and play them both together.
+  var source = context.createBufferSource();
+  for (var i=0; i < bufferList.length; i++) {
+    source.buffer = bufferList[i];
+    source.connect(context.destination);
+    source.start(0);
+  }
 }
