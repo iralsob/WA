0b9d5f2777ab3ae2994fcbc10310aef0a6db84b2
iralsob <iralsob@gmail.com>
2015-08-09 22:08:18 +0300

    sequential audio

--- script.js
+++ script.js
@@ -74,12 +74,11 @@ function init() {
 }
 function finishedLoading(bufferList) {
   // Create two sources and play them both together.
-  var source1 = context.createBufferSource();
-  var source2 = context.createBufferSource();
-  source1.buffer = bufferList[0];
-  source2.buffer = bufferList[1];
-
-  source1.connect(source2);
-  source2.connect(context.destination);
-  source1.start(0);
+  var source;
+  for (var i=0; i < bufferList.length; i++) {
+    source = context.createBufferSource();
+    source.buffer = bufferList[i];
+    source.connect(context.destination);
+    source.start(i*20);
+  }
 }
